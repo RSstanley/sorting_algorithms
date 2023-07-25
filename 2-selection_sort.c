35cc5d01213c9aaf095ab540fa1e0f8b091410e4
HoOdpHarMxcisT <122929857+HoOdpHarMxcisT@users.noreply.github.com>
2023-07-25 15:23:09 +0200

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -2,10 +2,8 @@
 
 /**
  * selection_sort - sorts an array of integers in ascending order using the
- * Selection sort algorithm
  * @array: The array to sort
  * @size: The size of the array
- *
  */
 void selection_sort(int *array, size_t size)
 {
