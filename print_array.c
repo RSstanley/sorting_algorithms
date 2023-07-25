1dfc151243ee807f4ab5f5c08f00355c19d5dd58
HoOdpHarMxcisT <122929857+HoOdpHarMxcisT@users.noreply.github.com>
2023-07-25 15:27:43 +0200

    Update print_array.c

--- print_array.c
+++ print_array.c
@@ -3,9 +3,7 @@
 
 /**
  * print_array - Prints an array of integers
- *
  * @array: The array to be printed
- * @size: Number of elements in @array
  */
 void print_array(const int *array, size_t size)
 {
