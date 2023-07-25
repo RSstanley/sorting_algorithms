d055d9ecc8906ecd9cfe200e8d8c1cd9a00979a2
RSstanley <stanleyrudzani@gmail.com>
2023-07-25 16:18:39 +0200

    fix the code

--- deck.h
+++ deck.h
@@ -3,6 +3,7 @@
 
 /**
  * enum kind_e - suit of the card
+ *
  * @SPADE: 0, or spades
  * @HEART: 1, or hearts
  * @CLUB: 2, or clubs
@@ -18,7 +19,6 @@ typedef enum kind_e
 
 /**
  * struct card_s - card representation
- *
  * @value: Value of the card
  * From "Ace" to "King"
  * @kind: Kind of the card
@@ -31,6 +31,7 @@ typedef struct card_s
 
 /**
  * struct deck_node_s - Deck of card
+ *
  * @card: Pointer to the card of the node
  * @prev: Pointer to the previous node of the list
  * @next: Pointer to the next node of the list
