d94afdebd5a5d208a3a7e64f1dbbd5dc02e66370
HoOdpHarMxcisT <122929857+HoOdpHarMxcisT@users.noreply.github.com>
2023-07-25 15:14:48 +0200

    Update 100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -2,10 +2,7 @@
 
 /**
  * shell_sort - sorts an array of integers in ascending order using the
- * Shell sort algorithm
  * @array: array to sort
- * @size: size of the array to sort
- *
  * Return: void
  */
 void shell_sort(int *array, size_t size)
