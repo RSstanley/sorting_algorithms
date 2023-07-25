b49920245aab396cd8d41fc38910dde62260d8ea
HoOdpHarMxcisT <122929857+HoOdpHarMxcisT@users.noreply.github.com>
2023-07-25 15:27:16 +0200

    Update deck.h

--- deck.h
+++ deck.h
@@ -3,7 +3,6 @@
 
 /**
  * enum kind_e - suit of the card
- *
  * @SPADE: 0, or spades
  * @HEART: 1, or hearts
  * @CLUB: 2, or clubs
@@ -19,6 +18,7 @@ typedef enum kind_e
 
 /**
  * struct card_s - card representation
+ *
  * @value: Value of the card
  * From "Ace" to "King"
  * @kind: Kind of the card
@@ -31,7 +31,6 @@ typedef struct card_s
 
 /**
  * struct deck_node_s - Deck of card
- *
  * @card: Pointer to the card of the node
  * @prev: Pointer to the previous node of the list
  * @next: Pointer to the next node of the list
